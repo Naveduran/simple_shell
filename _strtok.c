334a9bd98272d211f8bd3096a35865f999d44a64
Edheramirez <edher.ramirez@outlook.es>
2021-04-10 14:22:56 -0500

    fix: _strtok.c

--- _strtok.c
+++ _strtok.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 /**
- * _strtok - 
+ * _strtok - It´s our version of strtok funcion.
+ * @line: It´s pointer to array we recive in getline.
+ * @delim: It´s characters we mark off string in parts.
 */
 char *_strtok(char *line, char *delim)

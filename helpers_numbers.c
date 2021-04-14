8c04ebe8856b4bd6b33488f82cb19926184d708d
Naveduran <naveduran@gmail.com>
2021-04-14 03:20:13 +0000

    [Add] reallocation of the function long_to_string

---
+++ helpers_numbers.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+
+   /**
+	* long_to_string - converts a number to a string.
+	* @number: number to be converten in an string.
+	* @string: buffer to save the number as string.
+	* @base: base to convert number
+	*
+	* Return: Nothing.
+	*/
+void long_to_string(long number, char *string, int base)
+{
+	int index = 0, inNegative = 0;
+	long cociente = number;
+	char letters[] = {"0123456789abcdef"};
+
+	if (cociente == 0)
+		string[index++] = '0';
+
+	if (string[0] == '-')
+		inNegative = 1;
+
+	while (cociente)
+	{
+		if (cociente < 0)
+			string[index++] = letters[-(cociente % base)];
+		else
+			string[index++] = letters[cociente % base];
+		cociente /= base;
+	}
+	if (inNegative)
+		string[index++] = '-';
+
+	string[index] = '\0';
+	str_reverse(string);
+}

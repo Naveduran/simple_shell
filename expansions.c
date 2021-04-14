2d3a4bc0ebdbd3b471f9da98453ae79ef6993b6e
Naveduran <naveduran@gmail.com>
2021-04-14 05:14:50 +0000

    [Add] functionality: expanding ~

--- expansions.c
+++ expansions.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * expansions - search for $? and $$ to include the errno and the shell
  * pid respectively
@@ -7,11 +8,8 @@
  */
 
 int expansions(data_of_program *data)
-{
-	int i = 0, number = 0;
-	char expansion[10] = {0};
-	char *delimiter = NULL;
-	char *original = NULL;
+{	int i = 0, number = 0;
+	char expansion[10] = {0}, *expansion2, *delimiter = NULL, *original = NULL;
 
 	if (data->input_line == NULL)
 		return (0);
@@ -20,25 +18,34 @@ int expansions(data_of_program *data)
 		if (data->input_line[i] == '#')
 		{
 			data->input_line = '\0';
-			break;
+			return (0);
 		}
 		if (data->input_line[i] == '$')
-		{
 			if (data->input_line[i + 1] == '?' || data->input_line[i + 1] == '$')
 			{
-				original = str_duplicate(data->input_line);
-				free(data->input_line);
 				delimiter = "$";
 				if (original[i + 1] == '?')
 					number = errno;
 				else
 					number = (int) getpid();
-				data->input_line = str_duplicate(_strtok(original, delimiter));
 				long_to_string((long) number, expansion, 10);
+				original = str_duplicate(data->input_line);
+				free(data->input_line);
+				data->input_line = str_duplicate(_strtok(original, delimiter));
 				data->input_line = str_concat(data->input_line, expansion);
 				data->input_line = str_concat(data->input_line, original + i + 2);
 				free(original);
 			}
+		if (data->input_line[i] == '~')
+		{
+			delimiter = "~";
+			expansion2 = env_get_key("HOME", data);
+			original = str_duplicate(data->input_line), free(data->input_line);
+			data->input_line = str_duplicate(_strtok(original, delimiter));
+			data->input_line = str_concat(data->input_line, expansion2);
+			data->input_line = str_concat(data->input_line, original + i + 2);
+			printf("newline = %s\n", data->input_line);
+			free(original);
 		}
 	}
 	return (0);

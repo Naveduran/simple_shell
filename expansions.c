1dbe24e91acd0b277066ef4d26a811a10354726a
jepez90 <ing.jersonperez@gmail.com>
2021-04-24 21:52:00 -0500

    [Fix] error in string with $ only

--- expansions.c
+++ expansions.c
@@ -31,16 +31,16 @@ void expand_variables(data_of_program *data)
 			buffer_add(line, expansion);
 			buffer_add(line, data->input_line + i + 2);
 		}
+		else if (line[i] == '$' && (line[i + 1] == ' ' || line[i + 1] == '\0'))
+			continue;
 		else if (line[i] == '$')
 		{
 			for (j = 1; line[i + j] && line[i + j] != ' '; j++)
 				expansion[j - 1] = line[i + j];
 			temp = env_get_key(expansion, data);
-			line[i] = '\0';
-			expansion[0] = '\0';
+			line[i] = '\0', expansion[0] = '\0';
 			buffer_add(expansion, line + i + j);
-			if (temp)
-				buffer_add(line, temp);
+			temp ? buffer_add(line, temp) : 1;
 			buffer_add(line, expansion);
 		}
 	if (!str_compare(data->input_line, line, 0))
